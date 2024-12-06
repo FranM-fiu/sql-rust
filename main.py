568e4bff1340298a9a16b691630cd6af185b88c9
getinihavecandy0 <getinihavecandy2@proton.me>
2024-12-06 15:25:07 -0500

    progress commit

--- main.py
+++ main.py
@@ -36,5 +36,30 @@ def make_user(user: str, pw: str, hst: str, db: str, charset: str, collation: st
     cnx.close()  # Get rid of root db connection
 
 
+def update_table_enrollment(
+    user: str, pw: str, hst: str, db: str, charset: str, collation: str
+):
+    cnx = mysql.connector.connect(
+        user=user,
+        password=pw,
+        host=hst,
+        charset=charset,  # I needed this, it wouldnt work otherwise
+        collation=collation,  # This too
+        database=db,
+    )
+
+    cursor = cnx.cursor()
+    cursor.execute("DROP TRIGGER update_table")
+    cursor.execute("""
+DELIMETER ||
+
+CREATE TRIGGER update_table
+BEFORE INSERT ON ENROLLMENT 
+FOR EACH ROW
+BEGIN
+    DECLARE total_points DECIMAL(5,2) DEFAULT 0;
+    """)
+
+
 if __name__ == "__main__":
     main()
